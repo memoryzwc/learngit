64be5fa1348e157c583343cd5a9e67c82098e7a3
zyt <1370589661@qq.com>
2023-10-08 00:47:46 +0800

    wrote a cpp file

---
+++ helloword.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp1
+//
+//  Created by zytz on 2023/10/8.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
